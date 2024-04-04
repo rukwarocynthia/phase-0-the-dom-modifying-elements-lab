f379c536384056a83ac48976fed039bc2c0c4ed8
rukwarocynthia4093@gmail.com <rukwarocynthia4093@gmail.com>
2024-04-04 16:12:44 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// Remove main#main from DOM
+document.querySelector('main#main').remove();
+
+// Create new header
+const newHeader = document.createElement('h1');
+
+// Set id of the header to victory
+newHeader.id = 'victory';
+
+// Set the text content of the new element
+newHeader.textContent = "YOUR-NAME is the champion";
+
